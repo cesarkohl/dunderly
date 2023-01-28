6a81b2035b5fab55634aa50e6ce2e3875a0dcc4c
Cesar Kohl <cesarkohl@gmail.com>
2023-01-28 10:57:43 -0300

    Cleanup

--- logging/index.ts
+++ logging/index.ts
@@ -1,12 +1,7 @@
 import Logging from "./src/Logging";
 import CloudWatchClient from "./src/CloudWatchClient";
 import Output from "../shared/Output.interface";
-import Bugsnag from '@bugsnag/js';
 import BugsnagClient from "../error-reporting/src/BugsnagClient";
-
-// Bugsnag.start({ apiKey: '6b1e0dac71f299d84e0b0a3fd429c9ce' });
-Bugsnag.start({ apiKey: process.env.BUGSNAG_API_KEY ?? '' });
-Bugsnag.notify("This is an error - 2");
 
 interface Event {
     message: string;
